2ff8cc3b37a3abb8d13ffa2d58c0bf77914b124d
Asmaa1411 <asmaasaadazez@gmail.com>
2025-05-19 15:44:26 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_structure';
+  title = 'angular-app';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { HomeComponent } from './components/home/home.component';
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'contact-form', component: ContactFormComponent}
+];
